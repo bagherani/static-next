0235bb8be5274165b171d2d7fa74d8cefe80b384
mohi bagherani <mohammad.bagherani@gmail.com>
2024-01-30 21:33:35 +0100

    assets address fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/static-next'
+  basePath: '/static-next',
+  assetPrefix: '/static-next',
 };
 
 export default nextConfig;
